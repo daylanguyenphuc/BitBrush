7e6d59b8dec27be9b7b79f60d7a531c300c056b1
phucn3463 <phucn3463@gmail.com>
2024-02-02 15:51:01 +0700

    Add a ranking page

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import '@fontsource/roboto/700.css';
 import Header from "./Header";
 import Footer from './Footer';
 
+import SignUpPage from "./SignUpPage";
 import Homepage from "./Homepage";
 import Marketplace from "./Marketplace";
 import Collection from "./Collection";
+import Ranking from "./Ranking";
 import Create from "./Create";
 import CreateNFT from "./CreateNFT";
 import CreateCollection from "./CreateCollection";
-import SignUpPage from "./SignUpPage";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
             <Route exact path="/signup" element={<SignUpPage />} />
             <Route exact path="/marketplace" element={<Marketplace />} />
             <Route exact path="/collection" element={<Collection />} />
+            <Route exact path="/ranking" element={<Ranking />} />
             <Route exact path="/create" element={<Create />} />
             <Route exact path="/create/createNFT" element={<CreateNFT />} />
             <Route exact path="/create/createCollection" element={<CreateCollection />} />
---
+++ src/Ranking.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Grid, Container, Typography, Table, TableHead, TableCell, TableContainer, TableRow, Paper, Avatar } from '@mui/material';
+
+const data = [
+  { rank: 1, artist: 'Artist 1', sales: 50, volume: '100 ETH' },
+  { rank: 2, artist: 'Artist 2', sales: 40, volume: '80 ETH' },
+  { rank: 3, artist: 'Artist 3', sales: 30, volume: '60 ETH' },
+  { rank: 4, artist: 'Artist 4', sales: 20, volume: '40 ETH' },
+  { rank: 5, artist: 'Artist 5', sales: 10, volume: '20 ETH' },
+];
+
+const Ranking = () => {
+
+  return (
+    <>
+    <Container maxWidth="xl" style={{ marginTop: '50px', marginBottom: '20px' }}>
+      <Typography variant="h1" >Rankings</Typography>
+      <Typography variant="h3" gutterBottom>Check out top ranking NFT artists on the NFT Marketplace updated in real-time.</Typography>
+    </Container>
+
+    <Container maxWidth="xl" style={{ marginTop: '100px', marginBottom: '200px' }}>
+      <TableContainer component={Paper}>
+        <Table>
+        <TableHead>
+            <TableRow>
+              <TableCell align="left">#</TableCell>
+              <TableCell align="left">Artist</TableCell>
+              <TableCell align="left">Sales</TableCell>
+              <TableCell align="left">Volume</TableCell>
+            </TableRow>
+          </TableHead>
+          {data.map((row) => (
+            <TableRow key={row.rank}>
+              <TableCell>{row.rank}</TableCell>
+              <TableCell style={{display: 'flex', alignItems: 'center' }}>
+                <Avatar alt={row.artist} src={`url_to_artist_avatar_${row.rank}`} />
+                <Typography variant="body2" gutterBottom style={{marginLeft: '10px' }}>{row.artist}</Typography>
+              </TableCell>
+              <TableCell>{row.sales}</TableCell>
+              <TableCell>{row.volume}</TableCell>
+            </TableRow>
+          ))}
+        </Table>
+      </TableContainer>
+    </Container>
+    </>
+  );
+};
+
+export default Ranking
